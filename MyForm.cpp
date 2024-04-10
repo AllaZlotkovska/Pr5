e4d30680aa36091dc23fe93aa0cc89e7ad996083
Алла <@DESKTOP-QMLP632>
2024-04-10 12:55:51 +0300

    Add project files.

---
+++ MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+#include <Windows.h>
+#include <math.h>
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+int main(array<String^>^ arg)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Pr5::MyForm form;// ім'я вашого проекту, наприклад, myproekt або ін.
+	Application::Run(% form);
+}
